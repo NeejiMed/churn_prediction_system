a6d41ec33966fcf187cc845c79a4e60dc2bb55ff
NeejiMed <mohamedneji2410@gmail.com>
2026-03-12 01:49:11 +0100

    Integrate Streamlit UI with FastAPI prediction API

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
-import joblib
-
-artifact = joblib.load("models/churn_model.pkl")
-model = artifact['model']
-features = artifact['features']
+import requests
 
 st.title("Customer Churn Prediction")
 
@@ -12,19 +8,25 @@ tenure = st.slider("Tenure (months)", 0, 72)
 monthly_charges = st.number_input("Monthly Charges")
 total_charges = st.number_input("Total Charges")
 
-input_data = pd.DataFrame(columns=features)
-input_data.loc[0] = 0
-input_data['tenure'] = tenure
-input_data['MonthlyCharges'] = monthly_charges
-input_data['TotalCharges'] = total_charges
-
 if st.button("Predict Churn"):
 
-    probability = model.predict_proba(input_data)[0][1]
+    input_data = {
+        "tenure": tenure,
+        "MonthlyCharges": monthly_charges,
+        "TotalCharges": total_charges
+    }
+
+    response = requests.post(
+        "http://api:8000/predict",
+        json=input_data
+    )
+
+    result = response.json()
+    probability = result["churn_probability"]
+
     st.write(f"Predicted Probability of Churn: {probability:.2f}")
-    print("probability: ", probability)
+
     if probability > 0.4:
         st.warning("⚠ High Churn Risk! Consider taking action to retain this customer.")
     else:
         st.success("✅ Low Churn Risk! This customer is likely to stay.")
-        
--- src/api/app.py
+++ src/api/app.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+import joblib
+import pandas as pd
 
-app = FastAPI(title="Churn Prediction API")
+app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Churn Prediction API is running"}
+artifact = joblib.load("models/churn_model.pkl")
+model = artifact["model"]
+features = artifact["features"]
+
+
+@app.post("/predict")
+def predict(data: dict):
+
+    input_df = pd.DataFrame(columns=features)
+    input_df.loc[0] = 0
+
+    input_df["tenure"] = data["tenure"]
+    input_df["MonthlyCharges"] = data["MonthlyCharges"]
+    input_df["TotalCharges"] = data["TotalCharges"]
+
+    probability = model.predict_proba(input_df)[0][1]
+
+    return {"churn_probability": float(probability)}
